6a2baa9e0a603db87949d1b13d274a717e4f80b7
sobie2 <sobie2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-09-02 14:40:47 +0000

    lab_debug submission

--- sobie2/lab_debug/sketchify.cpp
+++ sobie2/lab_debug/sketchify.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include "png.h"
-
+#include <iostream>
+using namespace std;
 //
 // sets up the output image
 PNG* setupOutput(int w, int h)
@@ -12,33 +13,40 @@ PNG* setupOutput(int w, int h)
 // Returns my favorite color
 RGBAPixel* myFavoriteColor(int intensity)
 {
-    RGBAPixel color;
-    color.red   = 0;
-    color.green = intensity / 2;
-    color.blue  = intensity;
-    return &color;
+    /* Color must point to memory on heap
+	because stack frame is torn down
+	after we exit this function */
+    RGBAPixel* color=new RGBAPixel();
+    color->red   = 0;
+    color->green = intensity / 2;
+    color->blue  = intensity;
+    return color;
 }
 
 void sketchify()
 {
     // Load in.png
-    PNG* original;
+    /* Pointer must point to memory on heap
+	before we dereference it */
+    PNG* original=new PNG();
 
+    //cout << "Reached line 28" << endl;
     original->readFromFile("in.png");
     int width = original->width();
     int height = original->height();
+    //cout << "Reached line 32" << endl;
 
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output=new PNG();
+    output=setupOutput(width, height);
 
     // Load our favorite color to color the outline
     RGBAPixel* myPixel = myFavoriteColor(192);
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (int y = 1; 0 < y < height; y++) {
-        for (int x = 1; 0 < x < width; x++) {
+    for (int y = 1; y > 0 && y < height; y++) {
+        for (int x = 1; x > 0 && x < width; x++) {
             // Calculate the pixel difference
             RGBAPixel* prev = (*original)(x - 1, y - 1);
             RGBAPixel* curr = (*original)(x, y);
@@ -50,7 +58,7 @@ void sketchify()
             // color the output pixel with my favorite color
             RGBAPixel* currOutPixel = (*output)(x, y);
             if (diff > 100)
-                currOutPixel = myPixel;
+                *currOutPixel = *myPixel;
         }
     }
 
