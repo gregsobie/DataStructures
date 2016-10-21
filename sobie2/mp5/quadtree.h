6b19a9c41463c36fbdd24ee953acc2d49455dbc4
sobie2 <sobie2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-10-21 04:08:13 +0000

    mp5.1 submission

--- sobie2/mp5/quadtree.h
+++ sobie2/mp5/quadtree.h
@@ -15,7 +15,19 @@
 class Quadtree
 {
   public:
+	Quadtree();
+	Quadtree(PNG const &source, int resolution);
+	Quadtree(Quadtree const &other);
+	~Quadtree();
 
+	Quadtree const &operator=(Quadtree const &other);
+	void buildTree(PNG const &source, int resolution);
+	RGBAPixel getPixel(int x, int y) const;
+	PNG decompress() const;
+	void clockwiseRotate();
+	void prune(int tolerance);
+	int pruneSize(int tolerance) const;
+	int idealPrune(int numLeaves) const;
   private:
     /**
      * A simple class representing a single node of a Quadtree.
@@ -31,7 +43,48 @@ class Quadtree
         QuadtreeNode* seChild; /**< pointer to southeast child */
 
         RGBAPixel element; /**< the pixel stored as this node's "data" */
+	int x;
+	int y;
+	int resolution;
+
+	/* Internal node constructor */
+	QuadtreeNode(int xposition, int yposition, int res){
+		x=xposition;
+		y=yposition;
+		resolution=res;
+		nwChild=neChild=swChild=seChild=NULL;
+	}
+
+	/* Leaf node constructor */
+	QuadtreeNode(int xposition, int yposition, int res, PNG &source){
+		x=xposition;
+		y=yposition;
+		resolution=res;
+		nwChild=neChild=swChild=seChild=NULL;
+		element=*(source(x,y));
+	}
+
+	/* Copy node constructor */
+	QuadtreeNode(QuadtreeNode const *other){
+		x=other->x;
+		y=other->y;
+		resolution=other->resolution;
+		nwChild=neChild=swChild=seChild=NULL;
+		element=other->element;
+	}
     };
+
+	void clear(QuadtreeNode* &currNode);
+	QuadtreeNode* copy(QuadtreeNode* other);
+	void build(PNG const &source, int res, QuadtreeNode* currNode);
+	RGBAPixel getPixelHelper(int x, int y, QuadtreeNode* currNode) const;
+	int findQuadrant(int x, int y, QuadtreeNode* currNode) const;
+	void clockwiseRotate(QuadtreeNode* currNode);
+	void prune(int tolerance, QuadtreeNode* currNode);
+	int difference(QuadtreeNode* a, QuadtreeNode* b) const;
+	int pruneSize(int tolerance, QuadtreeNode* currNode) const;
+	int idealPrune(int numLeaves, int n, int interval) const;
+	
 
     QuadtreeNode* root; /**< pointer to root of quadtree */
 
