1b9e3c9f4724d052d7f4d5baea5b98c7a20057c4
sobie2 <sobie2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-11-16 04:06:19 +0000

    mp6 submission

--- sobie2/mp6/maptiles.cpp
+++ sobie2/mp6/maptiles.cpp
@@ -15,6 +15,27 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
-    return NULL;
-}
+    MosaicCanvas * mapTiles = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+    vector<Point<3>> avgColors;
+    map<Point<3>, TileImage> avgColorMap;
+    RGBAPixel temp;
+    for (size_t i=0; i<theTiles.size(); i++)
+    {
+    temp = theTiles[i].getAverageColor();
+        Point<3> color(temp.red, temp.green, temp.blue);
+    avgColorMap[color] = theTiles[i];
+        avgColors.push_back(color);
+    }
 
+    KDTree<3> tempTree(avgColors);
+    for (int i=0; i<theSource.getRows(); i++) {
+    for (int j=0; j<theSource.getColumns(); j++) {
+        temp = theSource.getRegionColor(i, j);
+        Point<3> point(temp.red, temp.green, temp.blue);
+        Point<3> nearest = tempTree.findNearestNeighbor(point);
+        mapTiles->setTile(i, j, avgColorMap[nearest]);
+        }
+    }
+
+    return mapTiles;
+} 
