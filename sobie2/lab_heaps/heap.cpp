bacf712f73f155404367fa5af0d06bbc79249a17
sobie2 <sobie2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-11-28 23:21:01 +0000

    lab_heaps updated

--- sobie2/lab_heaps/heap.cpp
+++ sobie2/lab_heaps/heap.cpp
@@ -1,3 +1,4 @@
+
 /**
  * @file heap.cpp
  * Implementation of a heap class.
@@ -43,11 +44,11 @@ size_t heap<T, Compare>::maxPriorityChild(size_t currentIdx) const
 {
     /// @todo Update to return the index of the child with highest priority
     ///   as defined by higherPriority()
-	size_t left=leftChild(currentIdx);
-	size_t right=rightChild(currentIdx);
-	if(hasAChild(currentIdx))
-		return higherPriority(_elems[left], _elems[right]) ? left:right;
-	else return 2*currentIdx;
+    size_t left=leftChild(currentIdx);
+    size_t right=rightChild(currentIdx);
+    if(rightChild(currentIdx) < _elems.size())
+        return higherPriority(_elems[left], _elems[right]) ? left:right;
+    else return 2*currentIdx;
 }
 
 template <class T, class Compare>
@@ -55,11 +56,11 @@ void heap<T, Compare>::heapifyDown(size_t currentIdx)
 {
     /// @todo Implement the heapifyDown algorithm.
     if (hasAChild(currentIdx)){
-    	size_t childIdx = maxPriorityChild(currentIdx);
-    	if (higherPriority(_elems[childIdx], _elems[currentIdx])){
-        	std::swap(_elems[currentIdx], _elems[childIdx]);
-        	heapifyDown(childIdx);
-    	}
+        size_t childIdx = maxPriorityChild(currentIdx);
+        if (higherPriority(_elems[childIdx], _elems[currentIdx])){
+            std::swap(_elems[currentIdx], _elems[childIdx]);
+            heapifyDown(childIdx);
+        }
     }
 }
 
@@ -80,29 +81,29 @@ heap<T, Compare>::heap()
 {
     /// @todo Depending on your implementation, this function may or may
     ///   not need modifying
-	_elems.push_back(0);
+    _elems.push_back(0);
 }
 
 template <class T, class Compare>
 heap<T, Compare>::heap(const std::vector<T>& elems)
 {
     /// @todo Construct a heap using the buildHeap algorithm
-	_elems.push_back(0);
-	for(int i=0; i<(int)elems.size(); i++)
-		_elems.push_back(elems[i]);
-	for(int j=parent(_elems.size()); j>0; j--)
-		heapifyDown(j);
+    _elems.push_back(0);
+    for(int i=0; i<(int)elems.size(); i++)
+        _elems.push_back(elems[i]);
+    for(int j=parent(_elems.size()); j>0; j--)
+        heapifyDown(j);
 }
 
 template <class T, class Compare>
 T heap<T, Compare>::pop()
 {
     /// @todo Remove, and return, the element with highest priority
-	T ret=_elems[1];
-	_elems[1]=_elems[_elems.size()-1];
-	_elems.pop_back();
-	heapifyDown(1);
-	return ret;
+    T ret=_elems[1];
+    _elems[1]=_elems[_elems.size()-1];
+    _elems.pop_back();
+    heapifyDown(1);
+    return ret;
 }
 
 template <class T, class Compare>
@@ -116,8 +117,8 @@ template <class T, class Compare>
 void heap<T, Compare>::push(const T& elem)
 {
     /// @todo Add elem to the heap
-	_elems.push_back(elem);
-	heapifyUp(_elems.size()-1);
+    _elems.push_back(elem);
+    heapifyUp(_elems.size()-1);
 }
 
 template <class T, class Compare>
