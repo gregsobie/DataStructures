fa72d28c43b59d589b25b7cba6fc675bb4772818
sobie2 <sobie2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-11-29 23:15:12 +0000

    mp7.1 submission

--- sobie2/mp7/dsets.h
+++ sobie2/mp7/dsets.h
@@ -1,1 +1,22 @@
 /* Your code here! */
+
+#ifndef DSETS_H
+#define DSETS_H
+
+#include <vector>
+using namespace std;
+
+class DisjointSets{
+    public:
+	//Creates 'num' unconnected root nodes, adds to end of uptree vector
+    	void addelements(int num);
+	//Searches for the uptree which contains elem, returning root index	
+    	int find(int elem);
+	//Combines uptrees if set indices differ
+    	void setunion(int a, int b);
+    private:
+	//disjointSet ADT
+    	vector<int> uptree;
+};
+
+#endif
